14ffbc4153f9ba6ff183f25a2b7ec2e0dfce206f
levitg <levitg@gmail.com>
2017-03-27 13:55:42 +0300

    reformat a bit, by PyCharm recommendations

--- codiity01-BinaryGap.py
+++ codiity01-BinaryGap.py
@@ -1,51 +1,48 @@
-#A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+# A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 
-#For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
+# For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
 
-#Write a function:
+# Write a function:
 
-#int solution(int N);
-#that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+# int solution(int N);
+# that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
 
-#For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
+# For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
 
-#Assume that:
+# Assume that:
 
-#N is an integer within the range [1..2,147,483,647].
-#Complexity:
+# N is an integer within the range [1..2,147,483,647].
+# Complexity:
 
-#expected worst-case time complexity is O(log(N));
-#expected worst-case space complexity is O(1).
+# expected worst-case time complexity is O(log(N));
+# expected worst-case space complexity is O(1).
 
-n =int("1041")
+n = int("1041")
+
 
 def solution(N):
-    b=list()
-    if N==0:
-        b=[0]
+    b = list()
+    if N == 0:
+        b = [0]
     else:
-        while(N>0):
-            b.append(N%2)
-            N = N//2
-    state=0
+        while N > 0:
+            b.append(N % 2)
+            N = N // 2
+    state = 0
     count = 0
     mcount = 0
     for i in b:
         if state == 0:
             if i == 1:
                 state = 1
-        if state == 1  :
+        if state == 1:
             if i == 0:
-                count+=1
+                count += 1
             elif i == 1:
-                if count > mcount : 
+                if count > mcount:
                     mcount = count
                 count = 0
     return mcount
 
 
-
 print(solution(n))
-
-                
-      
